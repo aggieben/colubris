1b05bb294f74c8478a157038b65f66021ec74a02
Benjamin Collins <ben.collins@acm.org>
2013-11-03 22:57:57 -0600

    use context + lambdas as a springboard to set up member function callbacks

--- Colubris.Svc/webhost_listener.cpp
+++ Colubris.Svc/webhost_listener.cpp
@@ -4,19 +4,16 @@
 colubris::webhost_listener::webhost_listener()
 {
 	init_callbacks();
+	_protocol_handle = 0;
 }
 
 bool colubris::webhost_listener::start()
 {
-	if (_current_context != nullptr)
+	HRESULT hr = WebhostRegisterProtocol(L"protocol", &_whl_callbacks, this, &this->_protocol_handle);
+	if (hr != S_OK)
 	{
-		OutputDebugString(L"unexpected context");
-		return false;
+		// handle error here
 	}
-	
-	_current_context = new context();
-	UuidCreate(&_current_context->id);
-	//HRESULT hr = WebhostRegisterProtocol(L"protocol", &_whl_callbacks, _current_context, )
 }
 
 bool colubris::webhost_listener::stop()
@@ -24,148 +21,164 @@ bool colubris::webhost_listener::stop()
 	return true;
 }
 
-void colubris::webhost_listener::on_cfgmgr_connected(void *context)
+void colubris::webhost_listener::on_cfgmgr_connected_impl()
 {
+
 }
 
-void colubris::webhost_listener::on_cfgmgr_disconnected(void *context, HRESULT hr)
+void colubris::webhost_listener::on_cfgmgr_disconnected_impl(HRESULT hr)
 {
+
 }
 
-void colubris::webhost_listener::on_cfgmgr_init_complete(void *context)
+void colubris::webhost_listener::on_cfgmgr_init_complete_impl()
 {
+
 }
 
-void colubris::webhost_listener::on_app_created(void *context, LPCWSTR app_key, LPCWSTR path, DWORD site_id, LPCWSTR apppool_id, PBYTE bindings, DWORD num_bindings, BOOL requests_blocked)
+void colubris::webhost_listener::on_app_created_impl(const wchar_t *app_key, const wchar_t *path, unsigned long site_id, const wchar_t *apppool_id, unsigned char *bindings, unsigned long num_bindings, bool requests_blocked)
 {
+
 }
 
-void colubris::webhost_listener::on_app_deleted(void *context, LPCWSTR app_key)
+void colubris::webhost_listener::on_app_deleted_impl(const wchar_t *app_key)
 {
+
 }
 
-void colubris::webhost_listener::on_app_bindings_changed(void *context, LPCWSTR app_key, PBYTE bindings, DWORD num_bindings)
+void colubris::webhost_listener::on_app_bindings_changed_impl(const wchar_t *app_key, unsigned char *bindings, unsigned long num_bindings)
 {
+
 }
 
-void colubris::webhost_listener::on_app_apppool_changed(void *context, LPCWSTR app_key, LPCWSTR apppool_id)
+void colubris::webhost_listener::on_app_apppool_changed_impl(const wchar_t *app_key, const wchar_t *apppool_id)
 {
+
 }
 
-void colubris::webhost_listener::on_app_requests_blocked_changed(void *context, LPCWSTR app_key, BOOL requests_blocked)
+void colubris::webhost_listener::on_app_requests_blocked_changed_impl(const wchar_t *app_key, bool requests_blocked)
 {
+
 }
 
-void colubris::webhost_listener::on_apppool_created(void *context, LPCWSTR apppool_id, PSID sid)
+void colubris::webhost_listener::on_apppool_created_impl(const wchar_t *apppool_id, void *sid)
 {
+
 }
 
-void colubris::webhost_listener::on_apppool_deleted(void *context, LPCWSTR appoool_id)
+void colubris::webhost_listener::on_apppool_deleted_impl(const wchar_t *appoool_id)
 {
+
 }
 
-void colubris::webhost_listener::on_apppool_identity_changed(void *context, LPCWSTR apppool_id, PSID sid)
+void colubris::webhost_listener::on_apppool_identity_changed_impl(const wchar_t *apppool_id, void *sid)
 {
+
 }
 
-void colubris::webhost_listener::on_apppool_state_changed(void *context, LPCWSTR apppool_id, BOOL enabled)
+void colubris::webhost_listener::on_apppool_state_changed_impl(const wchar_t *apppool_id, bool enabled)
 {
+
 }
 
-void colubris::webhost_listener::on_apppool_can_open_lci(void *context, LPCWSTR apppool_id, DWORD channel_id)
+void colubris::webhost_listener::on_apppool_can_open_lci_impl(const wchar_t *apppool_id, unsigned long channel_id)
 {
+
 }
 
-void colubris::webhost_listener::on_apppool_allstop(void *context, LPCWSTR apppool_id, DWORD channel_id)
+void colubris::webhost_listener::on_apppool_allstop_impl(const wchar_t *apppool_id, unsigned long channel_id)
 {
+
 }
 
 // Private member functions
 
 void colubris::webhost_listener::init_callbacks()
 {
-	_whl_callbacks.pfnWebhostListenerApplicationAppPoolChanged = &colubris::webhost_listener::on_app_apppool_changed;
-	_whl_callbacks.pfnWebhostListenerApplicationBindingsChanged = &colubris::webhost_listener::on_app_bindings_changed;
-	_whl_callbacks.pfnWebhostListenerApplicationCreated = &colubris::webhost_listener::on_app_created;
-	_whl_callbacks.pfnWebhostListenerApplicationDeleted = &colubris::webhost_listener::on_app_deleted;
-	_whl_callbacks.pfnWebhostListenerApplicationPoolAllListenerChannelInstancesStopped = &colubris::webhost_listener::on_apppool_allstop;
-	_whl_callbacks.pfnWebhostListenerApplicationPoolCanOpenNewListenerChannelInstance = &colubris::webhost_listener::on_apppool_can_open_lci;
-	_whl_callbacks.pfnWebhostListenerApplicationPoolCreated = &colubris::webhost_listener::on_apppool_created;
-	_whl_callbacks.pfnWebhostListenerApplicationPoolDeleted = &colubris::webhost_listener::on_apppool_deleted;
-	_whl_callbacks.pfnWebhostListenerApplicationPoolIdentityChanged = &colubris::webhost_listener::on_apppool_identity_changed;
-	_whl_callbacks.pfnWebhostListenerApplicationPoolStateChanged = &colubris::webhost_listener::on_apppool_state_changed;
-	_whl_callbacks.pfnWebhostListenerApplicationRequestsBlockedChanged = &colubris::webhost_listener::on_app_requests_blocked_changed;
-	_whl_callbacks.pfnWebhostListenerConfigManagerConnected = &colubris::webhost_listener::on_cfgmgr_connected;
-	_whl_callbacks.pfnWebhostListenerConfigManagerDisconnected = &colubris::webhost_listener::on_cfgmgr_disconnected;
-	_whl_callbacks.pfnWebhostListenerConfigManagerInitializationCompleted = &colubris::webhost_listener::on_cfgmgr_init_complete;
-}
+	// ConfigManager callbacks
+	_whl_callbacks.pfnWebhostListenerConfigManagerConnected = [](void *context)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_cfgmgr_connected_impl)();
+	};
 
-void colubris::webhost_listener::on_cfgmgr_connected_impl(void *context)
-{
+	_whl_callbacks.pfnWebhostListenerConfigManagerDisconnected = [](void *context, HRESULT hr)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_cfgmgr_disconnected_impl)(hr);
+	};
 
-}
+	_whl_callbacks.pfnWebhostListenerConfigManagerInitializationCompleted = [](void *context)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_cfgmgr_init_complete_impl)();
+	};
 
-void colubris::webhost_listener::on_cfgmgr_disconnected_impl(void *context, HRESULT hr)
-{
+	// Application callbacks
+	_whl_callbacks.pfnWebhostListenerApplicationCreated = [](void *context, LPCWSTR app_key, LPCWSTR path, DWORD site_id, LPCWSTR apppool_id, PBYTE bindings, DWORD num_bindings, BOOL requests_blocked)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_app_created_impl)(app_key, path, site_id, apppool_id, bindings, num_bindings, static_cast<bool>(requests_blocked));
+	};
 
-}
+	_whl_callbacks.pfnWebhostListenerApplicationDeleted = [](void *context, LPCWSTR app_key)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_app_deleted_impl)(app_key);
+	};
 
-void colubris::webhost_listener::on_cfgmgr_init_complete_impl(void *context)
-{
+	_whl_callbacks.pfnWebhostListenerApplicationBindingsChanged = [](void *context, LPCWSTR app_key, PBYTE bindings, DWORD num_bindings)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_app_bindings_changed_impl)(app_key, bindings, num_bindings);
+	};
 
-}
+	_whl_callbacks.pfnWebhostListenerApplicationAppPoolChanged = [](void *context, LPCWSTR app_key, LPCWSTR apppool_id)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_app_apppool_changed_impl)(app_key, apppool_id);
+	};
 
-void colubris::webhost_listener::on_app_created_impl(void *context, const wchar_t *app_key, const wchar_t *path, unsigned long site_id, const wchar_t *apppool_id, unsigned char *bindings, unsigned long num_bindings, bool requests_blocked)
-{
+	_whl_callbacks.pfnWebhostListenerApplicationRequestsBlockedChanged = [](void *context, LPCWSTR app_key, BOOL requests_blocked)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_app_requests_blocked_changed_impl)(app_key, requests_blocked);
+	};
 
-}
+	// AppPool Callbacks
+	_whl_callbacks.pfnWebhostListenerApplicationPoolCreated = [](void *context, LPCWSTR apppool_id, PSID sid)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_apppool_created_impl)(apppool_id, sid);
+	};
+	
+	_whl_callbacks.pfnWebhostListenerApplicationPoolDeleted = [](void *context, LPCWSTR apppool_id)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_apppool_deleted_impl)(apppool_id);
+	};
 
-void colubris::webhost_listener::on_app_deleted_impl(void *context, const wchar_t *app_key)
-{
+	_whl_callbacks.pfnWebhostListenerApplicationPoolIdentityChanged = [](void *context, LPCWSTR apppool_id, PSID sid)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_apppool_identity_changed_impl)(apppool_id, sid);
+	};
 
-}
+	_whl_callbacks.pfnWebhostListenerApplicationPoolStateChanged = [](void *context, LPCWSTR apppool_id, BOOL enabled)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_apppool_state_changed_impl)(apppool_id, enabled);
+	};
 
-void colubris::webhost_listener::on_app_bindings_changed_impl(void *context, const wchar_t *app_key, unsigned char *bindings, unsigned long num_bindings)
-{
+	_whl_callbacks.pfnWebhostListenerApplicationPoolCanOpenNewListenerChannelInstance = [](void *context, LPCWSTR apppool_id, DWORD channel_id)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_apppool_can_open_lci_impl)(apppool_id, channel_id);
+	};
 
-}
-
-void colubris::webhost_listener::on_app_apppool_changed_impl(void *context, const wchar_t *app_key, const wchar_t *apppool_id)
-{
-
-}
-
-void colubris::webhost_listener::on_app_requests_blocked_impl(void *context, const wchar_t *app_key, bool requests_blocked)
-{
-
-}
-
-void colubris::webhost_listener::on_apppool_created_impl(void *context, const wchar_t *apppool_id)
-{
-
-}
-
-void colubris::webhost_listener::on_apppool_deleted_impl(void *context, const wchar_t *appoool_id)
-{
-
-}
-
-void colubris::webhost_listener::on_apppool_identity_changed_impl(void *context, const wchar_t *apppool_id, void *sid)
-{
-
-}
-
-void colubris::webhost_listener::on_apppool_state_changed_impl(void *context, const wchar_t *apppool_id, bool enabled)
-{
-
-}
-
-void colubris::webhost_listener::on_apppool_can_open_lci_impl(void *context, const wchar_t *apppool_id, unsigned long channel_id)
-{
-
-}
-
-void colubris::webhost_listener::on_apppool_allstop_impl(void *context, const wchar_t *apppool_id, unsigned long channel_id)
-{
-
+	_whl_callbacks.pfnWebhostListenerApplicationPoolAllListenerChannelInstancesStopped = [](void *context, LPCWSTR apppool_id, DWORD channel_id)
+	{
+		auto _this = static_cast<colubris::webhost_listener *>(context);
+		(_this->*&colubris::webhost_listener::on_apppool_allstop_impl)(apppool_id, channel_id);
+	};
 }
--- Colubris.Svc/webhost_listener.hpp
+++ Colubris.Svc/webhost_listener.hpp
@@ -9,48 +9,26 @@ namespace colubris {
 		bool start();
 		bool stop();
 
-		static void on_cfgmgr_connected(void *context);
-		static void on_cfgmgr_disconnected(void *context, HRESULT hr);
-		static void on_cfgmgr_init_complete(void *context);
+		void on_cfgmgr_connected_impl();
+		void on_cfgmgr_disconnected_impl(HRESULT hr);
+		void on_cfgmgr_init_complete_impl();
 
-		static void on_app_created(void *context, LPCWSTR app_key, LPCWSTR path, DWORD site_id, LPCWSTR apppool_id, PBYTE bindings, DWORD num_bindings, BOOL requests_blocked);
-		static void on_app_deleted(void *context, LPCWSTR app_key);
-		static void on_app_bindings_changed(void *context, LPCWSTR app_key, PBYTE bindings, DWORD num_bindings);
-		static void on_app_apppool_changed(void *context, LPCWSTR app_key, LPCWSTR apppool_id);
-		static void on_app_requests_blocked_changed(void *context, LPCWSTR app_key, BOOL requests_blocked);
-		
-		static void on_apppool_created(void *context, LPCWSTR apppool_id, PSID sid);
-		static void on_apppool_deleted(void *context, LPCWSTR appoool_id);
-		static void on_apppool_identity_changed(void *context, LPCWSTR apppool_id, PSID sid);
-		static void on_apppool_state_changed(void *context, LPCWSTR apppool_id, BOOL enabled);
-		static void on_apppool_can_open_lci(void *context, LPCWSTR apppool_id, DWORD channel_id);
-		static void on_apppool_allstop(void *context, LPCWSTR apppool_id, DWORD channel_id);
+		void on_app_created_impl(const wchar_t *app_key, const wchar_t *path, unsigned long site_id, const wchar_t *apppool_id, unsigned char *bindings, unsigned long num_bindings, bool requests_blocked);
+		void on_app_deleted_impl(const wchar_t *app_key);
+		void on_app_bindings_changed_impl(const wchar_t *app_key, unsigned char *bindings, unsigned long num_bindings);
+		void on_app_apppool_changed_impl(const wchar_t *app_key, const wchar_t *apppool_id);
+		void on_app_requests_blocked_changed_impl(const wchar_t *app_key, bool requests_blocked);
+
+		void on_apppool_created_impl(const wchar_t *apppool_id, void *sid);
+		void on_apppool_deleted_impl(const wchar_t *appoool_id);
+		void on_apppool_identity_changed_impl(const wchar_t *apppool_id, void *sid);
+		void on_apppool_state_changed_impl(const wchar_t *apppool_id, bool enabled);
+		void on_apppool_can_open_lci_impl(const wchar_t *apppool_id, unsigned long channel_id);
+		void on_apppool_allstop_impl(const wchar_t *apppool_id, unsigned long channel_id);
 
 	private:
-		typedef struct context
-		{
-			UUID id;
-			DWORD protocol_handle;
-		};
-
 		WEBHOST_LISTENER_CALLBACKS _whl_callbacks;
-		context *_current_context;
-
+		unsigned long _protocol_handle;
 		void init_callbacks();
-
-		void on_cfgmgr_connected_impl(void *context);
-		void on_cfgmgr_disconnected_impl(void *context, HRESULT hr);
-		void on_cfgmgr_init_complete_impl(void *context);
-		void on_app_created_impl(void *context, const wchar_t *app_key, const wchar_t *path, unsigned long site_id, const wchar_t *apppool_id, unsigned char *bindings, unsigned long num_bindings, bool requests_blocked);
-		void on_app_deleted_impl(void *context, const wchar_t *app_key);
-		void on_app_bindings_changed_impl(void *context, const wchar_t *app_key, unsigned char *bindings, unsigned long num_bindings);
-		void on_app_apppool_changed_impl(void *context, const wchar_t *app_key, const wchar_t *apppool_id);
-		void on_app_requests_blocked_impl(void *context, const wchar_t *app_key, bool requests_blocked);
-		void on_apppool_created_impl(void *context, const wchar_t *apppool_id);
-		void on_apppool_deleted_impl(void *context, const wchar_t *appoool_id);
-		void on_apppool_identity_changed_impl(void *context, const wchar_t *apppool_id, void *sid);
-		void on_apppool_state_changed_impl(void *context, const wchar_t *apppool_id, bool enabled);
-		void on_apppool_can_open_lci_impl(void *context, const wchar_t *apppool_id, unsigned long channel_id);
-		void on_apppool_allstop_impl(void *context, const wchar_t *apppool_id, unsigned long channel_id);
 	};
 }
